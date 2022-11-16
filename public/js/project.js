b25d86d33fcdd2a7ddf9cddbb8d1a6e067fa5021
Katherine Lim <k4lim@ucsd.edu>
2022-11-15 17:22:36 -0800

    moved js to project.js

--- public/js/project.js
+++ public/js/project.js
@@ -10,3 +10,13 @@ function initializePage() {
 	// probably don't need anything yet
     console.log("boom baby");
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function expandHamburg() {
+	var x = document.getElementById("myTopnav");
+	 if (x.className === "navbar") {
+	   x.className += " responsive";
+	 } else {
+	   x.className = "navbar";
+	 }
+}
