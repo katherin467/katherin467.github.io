93cafe24373dd9345a357dec119fd9a2cff9d7bb
Katherine Lim <k4lim@ucsd.edu>
2022-11-22 18:07:49 -0800

    navbar hides on scroll!

--- public/js/project.js
+++ public/js/project.js
@@ -11,12 +11,28 @@ function initializePage() {
     console.log("boom baby");
 }
 
+var hamburgOpen = false;
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function expandHamburg() {
 	var x = document.getElementById("myTopnav");
-	 if (x.className === "navbar") {
-	   x.className += " responsive";
-	 } else {
-	   x.className = "navbar";
-	 }
+	if (x.className === "navbar") {
+		x.className += " responsive";
+		hamburgOpen = true;
+	} else {
+		x.className = "navbar";
+		hamburgOpen = false;
+	}
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+	var currentScrollPos = window.pageYOffset;
+	if (prevScrollpos > currentScrollPos) {
+		document.getElementById("myTopnav").style.top = "0";
+	} else {
+		if (hamburgOpen == false && currentScrollPos > 100) {
+		document.getElementById("myTopnav").style.top = "-100px";}
+	}
+	prevScrollpos = currentScrollPos;
 }
